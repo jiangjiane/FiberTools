dc75c50d924c3cf3065c6aeb4ea81e9852ed700e
jiangjiae <jiangjiane@outlook.com>
2017-07-28 20:23:52 +0800

    remove non_cc fibers

---
+++ remove_non_cc.py
@@ -0,0 +1,31 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from nibabel import streamlines
+import nibabel.streamlines.tck as nibtck
+import nibabel.streamlines.array_sequence as nibAS
+
+
+# load tck data
+img_cc = nibtck.TckFile.load('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
+                             'response_dhollander/100206/result/CC_fib.tck')
+streamstck = img_cc.streamlines
+# print streamstck[0][0][0]
+# print type(streamstck)
+
+L_temp = nibAS.ArraySequence()
+
+# remove non_CC
+for i in range(len(img_cc.streamlines)):
+    for j in range(len(img_cc.streamlines[i])):
+        if -2 < img_cc.streamlines[i][j][0] < 2 and img_cc.streamlines[i][j][2] > -10:  # -2<x<2 & z>-10
+            L_temp.append(img_cc.streamlines[i])
+            # pass
+print L_temp
+
+tractogram = streamlines.tractogram.Tractogram(streamlines=L_temp, data_per_streamline=img_cc.tractogram.data_per_streamline,
+                                               data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
+datdat = nibtck.TckFile(tractogram=tractogram, header=img_cc.header)
+datdat.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
+            'response_dhollander/100206/result/CC_fib_remove_non_cc_z-10.tck')
