c327fcaaecd914ddb18ca71b05d04d55240999db
jiangjiae <jiangjiane@outlook.com>
2017-07-29 16:56:09 +0800

    improve x-coordinate extract

--- remove_non_cc.py
+++ remove_non_cc.py
@@ -18,14 +18,17 @@ L_temp = nibAS.ArraySequence()
 
 # remove non_CC
 for i in range(len(img_cc.streamlines)):
+    l_x = []
     for j in range(len(img_cc.streamlines[i])):
-        if -2 < img_cc.streamlines[i][j][0] < 2 and img_cc.streamlines[i][j][2] > -10:  # -2<x<2 & z>-10
-            L_temp.append(img_cc.streamlines[i])
-            # pass
+        l_x.append(np.abs(img_cc.streamlines[i][j][0]))
+    x_min_index = np.argmin(l_x)
+    if img_cc.streamlines[i][x_min_index][2] > -10:  # -2<x<2 & z>-10
+        L_temp.append(img_cc.streamlines[i])
+        # pass
 print L_temp
 
 tractogram = streamlines.tractogram.Tractogram(streamlines=L_temp, data_per_streamline=img_cc.tractogram.data_per_streamline,
                                                data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
 datdat = nibtck.TckFile(tractogram=tractogram, header=img_cc.header)
 datdat.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
-            'response_dhollander/100206/result/CC_fib_remove_non_cc_z-10.tck')
+            'response_dhollander/100206/result/CC_fib_remove_non_cc_z-10_x-min.tck')
