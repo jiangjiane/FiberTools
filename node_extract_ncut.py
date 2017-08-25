63c7a1bad9d82b612210841cdb12dc8cce7587eb
jiangjiae <jiangjiane@outlook.com>
2017-08-25 22:17:10 +0800

    add clustering fiber

--- node_extract_ncut.py
+++ node_extract_ncut.py
@@ -8,18 +8,16 @@ import nibabel.streamlines.array_sequence as nibAS
 from scipy.spatial.distance import pdist, squareform
 
 import sys
-# from numpy import array, reshape, shape, matrix, ones, zeros, sqrt
-# from numpy import argsort, sign, kron, multiply, divide, abs, asarray
 from scipy import rand
 from scipy.sparse import csc_matrix, spdiags
 from scipy.sparse.linalg import eigsh
 from scipy.linalg import norm, svd, LinAlgError
 
 
-# img_cc = nibtck.TckFile.load('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
-#                              'response_dhollander/100206/result/CC_fib.tck')
 img_cc = nibtck.TckFile.load('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
-                             'response_dhollander/100206/result/CC_fib_only2_2.tck')
+                             'response_dhollander/100206/result/CC_fib.tck')
+# img_cc = nibtck.TckFile.load('/home/brain/workingdir/data/dwi/hcp/preprocessed/'
+#                              'response_dhollander/100206/result/CC_fib_only2_2.tck')
 streamstck = img_cc.streamlines
 
 # print streamstck[0]
@@ -79,33 +77,33 @@ def ncut( W, nbEigenValues ):
        exit(0)
 
     # degrees and regularization
-    d = abs(W).sum(0)
+    d = np.abs(W).sum(0)
     dr = 0.5*(d-W.sum(0))
     d = d+offset*2
     dr = dr+offset
 
     # calculation of the normalized LaPlacian
-    W = W+spdiags(dr,[0],m,m,"csc")
-    Dinvsqrt = spdiags((1.0/np.sqrt(d+eps)),[0],m,m,"csc")
-    P = Dinvsqrt*(W*Dinvsqrt);
+    W = W+spdiags(dr, [0], m, m, "csc")
+    Dinvsqrt = spdiags((1.0/np.sqrt(d+eps)), [0], m, m, "csc")
+    P = Dinvsqrt*(W*Dinvsqrt)
 
     # perform the eigen decomposition
     # eigen_val,eigen_vec = eigsh(P,nbEigenValues,maxiter=maxiterations,\
     #     tol=eigsErrorTolerence,which='LA')
-    eigen_val, eigen_vec = eigsh(P, nbEigenValues)
+    eigen_val, eigen_vec = eigsh(P, nbEigenValues, maxiter=maxiterations, tol=eigsErrorTolerence, which='LM')
 
     # sort the eigen_vals so that the first is the largest
     i = np.argsort(-eigen_val)
     eigen_val = eigen_val[i]
-    eigen_vec = eigen_vec[:,i]
+    eigen_vec = eigen_vec[:, i]
 
     # normalize the returned eigenvectors
     eigen_vec = Dinvsqrt*np.matrix(eigen_vec)
-    norm_ones = norm(np.ones((m,1)))
-    for i in range(0,eigen_vec.shape[1]):
-        eigen_vec[:,i] = (eigen_vec[:,i] / norm(eigen_vec[:,i]))*norm_ones
-        if eigen_vec[0,i] != 0:
-            eigen_vec[:,i] = -1 * eigen_vec[:,i] * np.sign( eigen_vec[0,i] )
+    norm_ones = norm(np.ones((m, 1)))
+    for i in range(0, eigen_vec.shape[1]):
+        eigen_vec[:, i] = (eigen_vec[:, i] / norm(eigen_vec[:, i]))*norm_ones
+        if eigen_vec[0, i] != 0:
+            eigen_vec[:, i] = -1 * eigen_vec[:, i] * np.sign(eigen_vec[0, i])
 
     return eigen_val, eigen_vec
 
@@ -115,9 +113,9 @@ def discretisation( eigen_vec ):
     eps = 2.2204e-16
 
     # normalize the eigenvectors
-    [n,k] = eigen_vec.shape
-    vm = np.kron(np.ones((1,k)),np.sqrt(np.multiply(eigen_vec,eigen_vec).sum(1)))
-    eigen_vec = np.divide(eigen_vec,vm)
+    [n, k] = eigen_vec.shape
+    vm = np.kron(np.ones((1, k)), np.sqrt(np.multiply(eigen_vec, eigen_vec).sum(1)))
+    eigen_vec = np.divide(eigen_vec, vm)
 
     svd_restarts = 0
     exitLoop = 0
@@ -126,13 +124,13 @@ def discretisation( eigen_vec ):
     while (svd_restarts < 30) and (exitLoop==0):
 
         # initialize algorithm with a random ordering of eigenvectors
-        c = np.zeros((n,1))
-        R = np.matrix(np.zeros((k,k)))
-        R[:,0] = eigen_vec[int(rand(1)*(n-1)),:].transpose()
+        c = np.zeros((n, 1))
+        R = np.matrix(np.zeros((k, k)))
+        R[:, 0] = eigen_vec[int(rand(1)*(n-1)), :].transpose()
 
-        for j in range(1,k):
-            c = c+abs(eigen_vec*R[:,j-1])
-            R[:,j] = eigen_vec[c.argmin(),:].transpose()
+        for j in range(1, k):
+            c = c+np.abs(eigen_vec*R[:, j-1])
+            R[:, j] = eigen_vec[c.argmin(), :].transpose()
 
 
         lastObjectiveValue = 0
@@ -152,9 +150,9 @@ def discretisation( eigen_vec ):
 
             # discretise the result by setting the max of each row=1 and
             # other values to 0
-            j=np.reshape(np.asarray(tDiscrete.argmax(1)),n)
-            eigenvec_discrete = csc_matrix((np.ones(len(j)),(range(0,n), \
-                np.array(j))),shape=(n,k))
+            j = np.reshape(np.asarray(tDiscrete.argmax(1)), n)
+            eigenvec_discrete = csc_matrix((np.ones(len(j)), (range(0, n), \
+                np.array(j))), shape=(n, k))
 
             # calculate a rotation to bring the discrete eigenvectors cluster to
             # the original eigenvectors
@@ -170,9 +168,9 @@ def discretisation( eigen_vec ):
 
             # test for convergence
             NcutValue = 2*(n-S.sum())
-            if((abs(NcutValue-lastObjectiveValue) < eps ) or \
-                      ( nbIterationsDiscretisation > \
-                        nbIterationsDiscretisationMax )):
+            if((np.abs(NcutValue-lastObjectiveValue) < eps) or \
+                      (nbIterationsDiscretisation > \
+                        nbIterationsDiscretisationMax)):
                 exitLoop = 1
             else:
                 # otherwise calculate rotation and continue
@@ -194,3 +192,47 @@ eigenvec_discrete = np.array(eigenvec_discrete)
 label_img = np.argmax(eigenvec_discrete, axis=1)
 
 print label_img
+
+d = zip(label_img, Ls_temp)
+
+# print d[0]
+# print d[1]
+# print d[0][0]
+# print d[0][1]
+print len(d)
+# print type(d)
+
+L_temp_0 = nibAS.ArraySequence()
+L_temp_1 = nibAS.ArraySequence()
+L_temp_2 = nibAS.ArraySequence()
+L_temp_3 = nibAS.ArraySequence()
+
+for k in range(len(d)):
+    if d[k][0] == 0:
+        L_temp_0.append(img_cc.streamlines[k])
+    if d[k][0] == 1:
+        L_temp_1.append(img_cc.streamlines[k])
+    if d[k][0] == 2:
+        L_temp_2.append(img_cc.streamlines[k])
+    else:
+        L_temp_3.append(img_cc.streamlines[k])
+
+tractogram = streamlines.tractogram.Tractogram(streamlines=L_temp_0, data_per_streamline=img_cc.tractogram.data_per_streamline,
+                                               data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
+datdat = nibtck.TckFile(tractogram=tractogram, header=img_cc.header)
+datdat.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/response_dhollander/100206/result/CC_fib_ncut_0.tck')
+
+tractogram1 = streamlines.tractogram.Tractogram(streamlines=L_temp_1, data_per_streamline=img_cc.tractogram.data_per_streamline,
+                                               data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
+datdat1 = nibtck.TckFile(tractogram=tractogram1, header=img_cc.header)
+datdat1.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/response_dhollander/100206/result/CC_fib_ncut_1.tck')
+
+tractogram2 = streamlines.tractogram.Tractogram(streamlines=L_temp_2, data_per_streamline=img_cc.tractogram.data_per_streamline,
+                                               data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
+datdat2 = nibtck.TckFile(tractogram=tractogram2, header=img_cc.header)
+datdat2.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/response_dhollander/100206/result/CC_fib_ncut_2.tck')
+
+tractogram3 = streamlines.tractogram.Tractogram(streamlines=L_temp_3, data_per_streamline=img_cc.tractogram.data_per_streamline,
+                                               data_per_point=img_cc.tractogram.data_per_point, affine_to_rasmm=img_cc.tractogram.affine_to_rasmm)
+datdat3 = nibtck.TckFile(tractogram=tractogram3, header=img_cc.header)
+datdat3.save('/home/brain/workingdir/data/dwi/hcp/preprocessed/response_dhollander/100206/result/CC_fib_ncut_3.tck')
