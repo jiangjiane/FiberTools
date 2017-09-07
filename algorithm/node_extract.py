adc739083651a93024f7a4a0af37ac9146d086e8
jiangjiae <jiangjiane@outlook.com>
2017-09-07 11:13:00 +0800

    improve extract algorithm

--- algorithm/node_extract.py
+++ algorithm/node_extract.py
@@ -13,14 +13,15 @@ def xmin_extract(streams):
     Ls_temp = []
     for i in range(len(streams.streamlines)):
         l = []
-        for j in range(len(streams.streamlines[i])):
+        for j in range(len(streams.streamlines[i])-1):
             l.append(streams.streamlines[i][j][0])
-        count = True
         for k in range(len(l) - 1):
-            if l[k] * l[k + 1] < 0 and count:
+            if l[k] * l[k + 1] < 0:
                 if np.abs(l[k]) < np.abs(l[k + 1]):
                     Ls_temp.append(streams.streamlines[i][k])
                 else:
                     Ls_temp.append(streams.streamlines[i][k + 1])
-                count = False
+
+            elif l[k] == 0:
+                Ls_temp.append(streams.streamlines[i][k])
     return Ls_temp
